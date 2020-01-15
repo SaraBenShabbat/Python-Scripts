bccff8ccf8f78fc45c4ef3a1231dc127a84f1bf0
SaraBenShabbat <sarabenshabbat@gmail.com>
2020-01-15 20:57:03 +0200

    OK

---
+++ SequenceOperators.py
@@ -0,0 +1,15 @@
+print('There are {0} in month: {1}, {2} etc...'.format(31, 'Jan', 'Feb'))
+print("a"+"b"+"c")
+print("a\nb\nc")
+print("a, b, c")
+x = """a
+b
+c
+"""
+a = 45
+b = 15
+c = 3
+print(a - b / c)
+atr = "bhjgggjhffgfj"
+pri
+print(atr[0:-1:2])
