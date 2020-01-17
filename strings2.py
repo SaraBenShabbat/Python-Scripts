e78e2eb8b213069a8585ddca26462d6e14614d23
SaraBenShabbat <sarabenshabbat@gmail.com>
2020-01-17 15:59:05 +0200

    I've finished up to page 123 of the presentation.

--- strings2.py
+++ strings2.py
@@ -38,3 +38,15 @@ print(parrot[0:6:2])
 print(parrot[0:6:3])
 
 number = "9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
+
+"""
+I run over number by 'for char in number'; Then, I wanna filter the characters - so, I do it by
+'char if char not in seperators else ""', which mean - take the current char on condition that 
+it doesn't exist in the the string called seperators;
+All of the above can can be written only in join function, so - we do it bu concatenating empty
+string to desired string represented in join function.
+"""
+values = "".join(char if char not in seperators else "" for char in number).split()
+print([int(val) for val in values])
