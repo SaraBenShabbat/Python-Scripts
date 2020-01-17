60428b2ead97978c269a3e3f3801a95fc6f81c5b
SaraBenShabbat <sarabenshabbat@gmail.com>
2020-01-17 03:26:24 +0200

    OK

--- strings2.py
+++ strings2.py
@@ -1,1 +1,38 @@
-parrot = 
+parrot = "Norwegian Blue"
+
+print(parrot)
+print(parrot[3])
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print()
+
+print(parrot[-1])
+print(parrot[-14])
+
+print()
+
+print(parrot[3 - 14])
+print(parrot[4 - 14])
+print(parrot[9 - 14])
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+
+print(parrot[0:6])
+print(parrot[3:5])
+print(parrot[0:9])
+
+print(parrot[:9])
+print(parrot[10:14])
+
+print(parrot[10:])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[:6] + parrot[6:])
+print(parrot[:])
+
+print(parrot[0:6:2])
+print(parrot[0:6:3])
